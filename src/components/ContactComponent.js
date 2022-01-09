27d7e8c22c1df15a3a810d711098edb39b60cdca
Corey Benedict-Osborn <corey.benedict@gmail.com>
2022-01-09 16:21:02 -0500

    Week Four - React Redux Form Validation, first half

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import {
-  Breadcrumb, BreadcrumbItem, Button,
-  Label, Col, Row, } from "reactstrap";
+  Breadcrumb, BreadcrumbItem, 
+  Button, Label, Col, Row, } from "reactstrap";
 import { Link } from "react-router-dom";
-import { Control, LocalForm } from "react-redux-form";
+import { Control, LocalForm, Errors } from "react-redux-form";
+
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+const isNumber = val => !isNaN(+val);
+const validEmail = val => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+
 
 class Contact extends Component {
   constructor(props) {
@@ -94,6 +101,11 @@ class Contact extends Component {
                     name="firstName"
                     placeholder="First Name"
                     className="form-control"
+                    validators={{
+                      required, 
+                      minLength: minLength(2),
+                      maxLength: maxLength(15)
+                    }}
                   />
                 </Col>
               </Row>
@@ -111,6 +123,11 @@ class Contact extends Component {
                     name="lastName"
                     placeholder="Last Name"
                     className="form-control"
+                    validators={{
+                      required, 
+                      minLength: minLength(2),
+                      maxLength: maxLength(15)
+                    }}
                   />
                 </Col>
               </Row>
@@ -128,6 +145,12 @@ class Contact extends Component {
                     name="phoneNum"
                     placeholder="Phone number"
                     className="form-control"
+                    validators={{
+                      required, 
+                      minLength: minLength(10),
+                      maxLength: maxLength(15),
+                      isNumber
+                    }}
                   />
                 </Col>
               </Row>
@@ -145,6 +168,10 @@ class Contact extends Component {
                     name="email"
                     placeholder="Email"
                     className="form-control"
+                    validators={{
+                      required, 
+                      validEmail
+                    }}
                   />
                 </Col>
               </Row>
